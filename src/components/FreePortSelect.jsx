a3be32e33c241a7a29caa5d6936f4aa80941b62b
Bart Bakker <bart@thesoftwarecraft.com>
2017-11-07 16:04:39 +0100

    Show warning when no free ports are available

--- src/components/FreePortSelect.jsx
+++ src/components/FreePortSelect.jsx
@@ -3,6 +3,8 @@ import PropTypes from "prop-types";
 import Select from "react-select";
 import "react-select/dist/react-select.css";
 
+import "./FreePortSelect.css";
+
 import {freePortsForLocationCodeAndInterfaceType} from "../api";
 
 export default class FreePortSelect extends React.PureComponent {
@@ -10,31 +12,34 @@ export default class FreePortSelect extends React.PureComponent {
     constructor(props) {
         super(props);
         this.state = {
-            freePorts: []
+            freePorts: [],
+            loading: true
         }
     }
 
     componentWillMount() {
         freePortsForLocationCodeAndInterfaceType(this.props.locationCode, this.props.interfaceType).then(result =>
-            this.setState({freePorts: result})
+            this.setState({freePorts: result, loading: false})
         );
-
     };
 
     render() {
-        const {freePorts} = this.state;
+        const {freePorts, loading} = this.state;
         const {onChange, freePort, disabled} = this.props;
 
+        const noFreePortsAvailable = !loading && freePorts.length === 0;
+
         return (
-            <Select className="select-free-port"
-                    onChange={onChange}
-                    options={freePorts.map(aFreePort => {
-                        return {value: aFreePort.id, label: `${aFreePort.node}_${aFreePort.port}`};
-                    })}
-                    value={freePort}
-                    searchable={true}
-                    disabled={disabled || freePorts.length === 0}/>
-        )
+            <div className="free-port-select">
+                <Select className="select-free-port"
+                        onChange={onChange}
+                        options={freePorts.map(x => { return {value: x.id, label: `${x.node}_${x.port}`}; })}
+                        value={freePort}
+                        searchable={true}
+                        disabled={disabled || freePorts.length === 0} />
+                {noFreePortsAvailable && <em className="warn">No free ports available</em>}
+            </div>
+        );
     }
 
 }
