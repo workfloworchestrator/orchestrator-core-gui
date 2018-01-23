8c44dd78da23f99c0af6d89083f3441055382a16
oharsta <oharsta@zilverline.com>
2018-01-23 22:30:04 +0100

    Added tests for nested state / keys

--- src/__tests__/utils/NestedState.test.js
+++ src/__tests__/utils/NestedState.test.js
@@ -1,4 +1,4 @@
-import {lookupValueFromNestedState, findValueFromInputStep} from "../../utils/NestedState";
+import {findValueFromInputStep, lookupValueFromNestedState} from "../../utils/NestedState";
 
 test("Lookup existing key in process state", () => {
     const value = lookupValueFromNestedState("key", {key: "value"});
@@ -16,37 +16,37 @@ test("Lookup nested key in process state", () => {
 });
 
 test("Lookup non-existing nested key in process state", () => {
-    const value = lookupValueFromNestedState("sub.key.very.deeply.nested", {"nope":"value"});
+    const value = lookupValueFromNestedState("sub.key.very.deeply.nested", {"nope": "value"});
     expect(value).toBe(null)
 });
 
 test("Lookup nested key in flattened process state", () => {
-    const value = lookupValueFromNestedState("msp.location_code", {"msp.location_code":"value"});
+    const value = lookupValueFromNestedState("msp.location_code", {"msp.location_code": "value"});
     expect(value).toBe(null)
 });
 
 test("Lookup nested key in non-flattened process state", () => {
-    const value = lookupValueFromNestedState("msp.location_code", {"msp":{location_code:"value"}});
-    expect(value).toBe(value)
+    const value = lookupValueFromNestedState("msp.location_code", {"msp": {location_code: "value"}});
+    expect(value).toBe("value")
 });
 
 test("Lookup existing name in user input", () => {
-    const value = findValueFromInputStep("name", [{name: "name", value:"value"}]);
+    const value = findValueFromInputStep("name", [{name: "name", value: "value"}]);
     expect(value).toBe("value")
 });
 
 test("Lookup non-existing name in user input", () => {
-    const value = findValueFromInputStep("name", [{name: "nope", value:"value"}]);
+    const value = findValueFromInputStep("name", [{name: "nope", value: "value"}]);
     expect(value).toBe(null);
 });
 
 test("Lookup nested name in user input", () => {
-    const value = findValueFromInputStep("nested.name", [{name: "nested.name", value:"value"}]);
+    const value = findValueFromInputStep("nested.name", [{name: "nested.name", value: "value"}]);
     expect(value).toBe("value")
 });
 
 test("Lookup non-existing nested name in user input", () => {
-    const value = findValueFromInputStep("very.deep.nested.name", [{name: "nope", value:"value"}]);
+    const value = findValueFromInputStep("very.deep.nested.name", [{name: "nope", value: "value"}]);
     expect(value).toBe(null)
 });
 
