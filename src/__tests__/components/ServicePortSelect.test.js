7059d131fb9693a6de91f67f2063ad38e6b779ac
Rene Dohmen (imac) <acidjunk@gmail.com>
2018-04-17 17:59:00 +0200

    Added tests

--- src/__tests__/components/ServicePortSelect.test.js
+++ src/__tests__/components/ServicePortSelect.test.js
@@ -1,20 +1,48 @@
 import React from "react";
 import {shallow} from "enzyme";
-import UserProfile from "../../components/ServicePortSelect";
+import ServicePortSelect from "../../components/ServicePortSelect";
+import PropTypes from 'prop-types';
 import start from "../base";
 start();
 
 
-test("ServicePortSelect with email", () => {
-    const {onChange, servicePort, servicePorts, organisations, disabled} = this.props;
-    const servicePortSelect = shallow(
-        <ServicePortSelect key={index} onChange={this.onChangeInternal("subscription_id", index)}
-                           servicePort={servicePort.subscription_id}
-                           servicePorts={inSelect}
-                           organisations={organisations}
-                           disabled={disabled}/>
+test("ServicePortSelect with SSP", () => {
+    const servicePort = {'tag': 'SSP'};
+    const component_wrapper = shallow(
+        <ServicePortSelect onChange={PropTypes.func.isRequired}
+                           key={PropTypes.string.isRequired}
+                           servicePort={'368f7799-b22a-4dae-a499-d7ac199bb36f'}
+                           servicePorts={[]}
+                           organisations={[]}
+                           disabled={false}/>
     );
+    expect(component_wrapper.instance().is_selectable(servicePort)).toBe(true);
+});
 
-    expect(userProfile.contains(<span className="user-key">email</span>)).toBe(true);
 
+test("ServicePortSelect with 'out of sync' MSP", () => {
+    const servicePort = {'tag': 'MSP', 'insync': false};
+    const component_wrapper = shallow(
+        <ServicePortSelect onChange={PropTypes.func.isRequired}
+                           key={PropTypes.string.isRequired}
+                           servicePort={'368f7799-b22a-4dae-a499-d7ac199bb36f'}
+                           servicePorts={[]}
+                           organisations={[]}
+                           disabled={false}/>
+    );
+    expect(component_wrapper.instance().is_selectable(servicePort)).toBe(false);
+});
+
+
+test("ServicePortSelect with 'in sync' MSP", () => {
+    const servicePort = {'tag': 'MSP', 'insync': true};
+    const component_wrapper = shallow(
+        <ServicePortSelect onChange={PropTypes.func.isRequired}
+                           key={PropTypes.string.isRequired}
+                           servicePort={'368f7799-b22a-4dae-a499-d7ac199bb36f'}
+                           servicePorts={[]}
+                           organisations={[]}
+                           disabled={false}/>
+    );
+    expect(component_wrapper.instance().is_selectable(servicePort)).toBe(true);
 });
