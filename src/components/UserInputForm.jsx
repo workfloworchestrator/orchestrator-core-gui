8c3c6dccb9e4993a9dcb4c9c3b3e4c83c091e414
Guido Kollerie <guido@kollerie.com>
2018-09-03 10:40:21 +0200

    Make organisation and dienstafname readonly if preselected.
    When a process is started coming from CRM, `organisation` and `dienstafname`
    will be preselected. The URL will be:
    
    http[s]://<host:port>/new-process?product=<guid>&organisation=<guid>&dienstafname=<guid>
    
    The preselected fields should be readonly. For `products` that was already
    the case. For `organisation` and `dienstafname` that wasn't.

--- src/components/UserInputForm.jsx
+++ src/components/UserInputForm.jsx
@@ -255,7 +255,7 @@ export default class UserInputForm extends React.Component {
             case "ims_id":
             case "isalias":
             case "stp":
-                return <input type="text" id={name} name={name} value={value || ""}
+                return <input type="text" id={name} name={name} value={value || ""} readOnly={userInput.readonly}
                               onChange={this.changeStringInput(name)} onBlur={this.validateUserInput(name)}/>;
             case "subscription_id":
                 return <ReadOnlySubscriptionView subscriptionId={value}
