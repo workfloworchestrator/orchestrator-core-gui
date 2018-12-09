116322d94587a8ceffd012264c9a050fcf1f16d8
Paul Frederiks <pfrederiks@ebay.com>
2018-12-10 00:06:52 +0100

    Fix prefix tab performance issues
    Lessons learned:
     - don't debounce a promise
     - don't let a recount trigger a recount
     - renderDate is expensive
     - ipAddressToInt is really expensive
     - don't use filter and array length to count occurences

--- src/pages/Prefixes.jsx
+++ src/pages/Prefixes.jsx
@@ -5,7 +5,7 @@ import debounce from "lodash/debounce";
 import {isEmpty, stop, isValidUUIDv4} from "../utils/Utils";
 
 import "./Prefixes.css";
-import {freeSubnets, prefix_filters, prefixById, subscriptionsByProductType} from "../api";
+import {freeSubnets, prefix_filters, prefixSubscriptionsByRootPrefix} from "../api";
 import FilterDropDown from "../components/FilterDropDown";
 import {organisationNameByUuid, renderDate, ipamStates, ipAddressToNumber, familyFullName} from "../utils/Lookups";
 
@@ -15,6 +15,9 @@ export default class Prefixes extends React.PureComponent {
   constructor(props) {
     super(props);
 
+    this.debouncedCount = debounce(this.count, 1500, {leading: true, trailing: true});
+    this.debouncedRunQuery = debounce(this.runQuery, 800);
+
     this.state = {
       prefixes: [],
       query: "",
@@ -22,11 +25,11 @@ export default class Prefixes extends React.PureComponent {
       sortOrder: {name: "prefix", descending: false},
       filterAttributes: {
         state: ipamStates.filter(s => s).map(state =>
-          ({name: state, selected: true, count: 0})),
+          ({name: state, selected: (state === "Allocated"), count: 0})),
         rootPrefix: [],
         family: [
           {name: "IPv4", selected: true, count: 0},
-          {name: "IPv6", selected: true, count: 0}
+          {name: "IPv6", selected: false, count: 0}
         ]
       },
       rootPrefixes: [],
@@ -39,81 +42,61 @@ export default class Prefixes extends React.PureComponent {
     this.setState({})
     prefix_filters()
       .then(result => {
-          const prefixFilters = result.map(p => ({name: p.prefix, selected: true, count: 0}));
+          const prefixFilters = result.map((p, idx) => ({name: p.prefix, selected: (idx === 0), count: 0}));
           const currentFilterAttributes = this.state.filterAttributes;
           const modifiedAttributes = {rootPrefix: prefixFilters}
           this.setState({rootPrefixes: result, filterAttributes: {...currentFilterAttributes, ...modifiedAttributes}});
           this.getFreePrefixes(result);
+          this.getPrefixSubscriptions(result);
       });
-    subscriptionsByProductType("IP_PREFIX")
-      .then(subscriptions =>
-        subscriptions
-        .filter(s => s.status === "active")
-        .map(sub => this.enrichIPPrefixSubscription(sub)));
-  };
+   };
 
   componentDidUpdate(prevProps, prevState) {
       if (this.state.prefixes !== prevState.prefixes) {
-          this.count();
-      }
-      if (this.state.filterAttributes !== prevState.filterAttributes) {
-          this.count();
+          this.debouncedCount();
       }
   };
 
-  enrichIPPrefixSubscription(sub) {
-     const ipam_prefix_id = sub.instances
-        .map(instance =>
-            instance.values.filter(v => v.resource_type.resource_type === "ipam_prefix_id")
-            .map(v => v.value)[0])[0];
-     return prefixById(ipam_prefix_id)
-        .then(prefix => ({
-                id: prefix.id,
-                customer_name: organisationNameByUuid(sub.customer_id, this.props.organisations),
-                subscription_id: sub.subscription_id,
-                start_date: sub.start_date,
-                description: sub.description,
-                prefix: prefix.prefix,
-                prefixlen: parseInt(prefix.prefix.split("/")[1], 10),
-                parent: prefix.parent__label.split(": ")[1],
-                family: prefix.afi,
-                state: prefix.state})
-        )
-        .catch(error => ({
-             id: 9999,
-             customer_name: organisationNameByUuid(sub.customer_id, this.props.organisations),
-             subscription_id: sub.subscription_id,
-             start_date: sub.start_date,
-             description: sub.description,
-             state: ipamStates.indexOf("Failed"),
-             parent: "",
-             family: 0,
-             prefix: error.message,
-             prefixlen: 0
-        }))
-        .then(result => {
-            this.setState({prefixes: this.state.prefixes.concat(result)});
-        });
-
+   getPrefixSubscriptions = roots => {
+      const {organisations} = this.props;
+      return roots.map(p =>
+          prefixSubscriptionsByRootPrefix(p.id)
+          .then(result => result.map(prefix => {
+              const {customer_id, start_date, subscription_id} = prefix;
+              const organisation = customer_id === undefined ? "Unknown" : organisationNameByUuid(customer_id, organisations);
+              const subscription = subscription_id === undefined ? "Missing": subscription_id;
+              return {...prefix, customer_name: organisation, start_date_as_str: renderDate(start_date), subscription_id: subscription};
+          })) 
+            .then(result => {
+                this.setState(prevState => ({prefixes: prevState.prefixes.concat(result)}))
+            })
+      )
   };
 
   getFreePrefixes = roots => {
+        const now = Math.floor(Date.now() / 1000);
+        const nowString = renderDate(now);
         return roots.map(p =>
             freeSubnets(p.prefix)
                 .then(result => {
                   const {availablePrefixId} = this.state;
-                  const free = result.map((r, idx) => ({
-                      id: availablePrefixId + idx,
-                      customer_name: "N/A",
-                      subscription_id: "N/A",
-                      start_date: Math.floor(Date.now() / 1000),
-                      description: "Vrije ruimte - gegenereerd",
-                      family: p.version,
-                      prefix: r,
-                      prefixlen: parseInt(r.split("/")[1], 10),
-                      parent: p.prefix,
-                      state: ipamStates.indexOf("Free")
-                  }));
+                  const free = result.map((r, idx) => {
+                      const [networkAddress, prefixlen] = r.split("/"); 
+                      return {
+                        id: availablePrefixId + idx,
+                        customer_name: "N/A",
+                        subscription_id: "N/A",
+                        start_date: now,
+                        start_date_as_str: nowString,
+                        description: "Vrije ruimte - gegenereerd",
+                        family: p.version,
+                        prefix: r,
+                        network_address_as_int: ipAddressToNumber(networkAddress), 
+                        prefixlen: parseInt(prefixlen, 10),
+                        parent: p.prefix,
+                        state: ipamStates.indexOf("Free")
+                      };
+                  });
                   this.setState(prevState => ({
                       prefixes: prevState.prefixes.concat(free),
                       availablePrefixId: prevState.availablePrefixId + free.length
@@ -123,20 +106,34 @@ export default class Prefixes extends React.PureComponent {
   };
 
 
-  count = debounce(() => {
+  count = () => {
       const {prefixes, filterAttributes} = this.state;
       const {state, rootPrefix, family} = filterAttributes;
-      const stateCount = state.map(attr => (
-          {...attr, count: prefixes.filter(p => ipamStates[p.state] === attr.name).length})
-      );
-      const rootPrefixCount = rootPrefix.map(attr => (
-          {...attr, count: prefixes.filter(p => p.parent === attr.name).length})
-      );
-      const familyCount = family.map(attr => (
-          {...attr, count: prefixes.filter(p => familyFullName[p.family] === attr.name).length})
-      );
-      this.setState({filterAttributes: {state: stateCount, rootPrefix: rootPrefixCount, family: familyCount}})
-  }, 250);
+      const stateCount = state.map(attr => {
+          const newCount = prefixes.reduce((acc, p) => {
+              return ipamStates[p.state] === attr.name ? acc + 1: acc;
+          }, 0);
+          return newCount === attr.count? attr: {...attr, count: newCount};
+      });
+      const rootPrefixCount = rootPrefix.map(attr => {
+          const newCount = prefixes.reduce((acc, p) => {
+              return p.parent === attr.name ? acc + 1: acc;
+          }, 0);
+          return newCount === attr.count ? attr: {...attr, count: newCount};
+      });
+      const familyCount = family.map(attr => {
+          const newCount = prefixes.reduce((acc, p) => {
+              return familyFullName[p.family] === attr.name ? acc + 1: acc;
+          }, 0);
+         return newCount === attr.count? attr: {...attr, count: newCount};
+      });
+      this.setState({
+          filterAttributes: {
+              state: stateCount,
+              rootPrefix: rootPrefixCount,
+              family: familyCount}
+      });
+  };
 
   setFilter = filterName => item => {
     const currentFilterAttributes = this.state.filterAttributes;
@@ -202,7 +199,7 @@ export default class Prefixes extends React.PureComponent {
       const aSafe = a[name] === undefined ? "" : a[name];
       const bSafe = b[name] === undefined ? "" : b[name];
       if (name === "prefix") {
-        return ipAddressToNumber(aSafe.split("/")[0]) - ipAddressToNumber(bSafe.split("/")[0]);
+        return a["network_address_as_int"] - b["network_address_as_int"];
     } else if (name === "state") {
         return ipamStates[parseInt(aSafe,10)].localeCompare(ipamStates[parseInt(bSafe,10)]);
     } else {
@@ -230,28 +227,22 @@ export default class Prefixes extends React.PureComponent {
   search = e => {
     const query = e.target.value;
     this.setState({query: query});
-    this.delayedSearch(query);
+    this.debouncedRunQuery(query);
   };
 
-  delayedSearch = debounce(query => {
-    const {prefixes} = this.state;
-    const queryToLower = query.toLowerCase();
-    const runQuery = new Promise(
-        function(resolve, reject) {
-         const results = prefixes.filter(prefix => {
-                    return (prefix.prefix.toLowerCase().includes(queryToLower)
-                    || prefix.customer_name.toLowerCase().includes(queryToLower)
-                    || prefix.description.toLowerCase().includes(queryToLower)
-                    || ipamStates[prefix.state].toLowerCase().includes(queryToLower)
-                    || queryToLower === familyFullName[prefix.family].toLowerCase()
-                    || renderDate(prefix.start_date).includes(query))
-                });
-         resolve(results);
+  runQuery = query => {
+     const {prefixes} = this.state;
+     const queryToLower = query.toLowerCase();
+     const results = prefixes.filter(prefix => {
+                return (prefix.prefix.toLowerCase().includes(queryToLower)
+                || prefix.customer_name.toLowerCase().includes(queryToLower)
+                || prefix.description.toLowerCase().includes(queryToLower)
+                || ipamStates[prefix.state].toLowerCase().includes(queryToLower)
+                || queryToLower === familyFullName[prefix.family].toLowerCase()
+                || prefix.start_date_as_str.includes(query))
     });
-    runQuery.then(results => {
-        this.setState({searchResults: results});
-    });
-  }, 250);
+    this.setState({searchResults: results});
+  };
 
   sortColumnIcon = (name, sorted) => {
      if (sorted.name === name) {
@@ -259,8 +250,6 @@ export default class Prefixes extends React.PureComponent {
      }
      return <i/>;
   };
-
-  
 
   subscriptionLink = (selection)  => () => {
       const {subscription_id, prefix, prefixlen} = selection;
@@ -345,7 +334,7 @@ export default class Prefixes extends React.PureComponent {
         <td data-label={I18n.t("prefixes.state")}
           className="state">{ipamStates[prefix.state]}</td>
         <td data-label={I18n.t("prefixes.start_date")}
-          className="start_date">{renderDate(prefix.start_date)}</td>
+          className="start_date">{prefix.start_date_as_str}</td>
       </tr>
       )
     }
