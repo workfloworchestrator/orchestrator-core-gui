e6fd950bf83e6bb8876cb939e08cf47ba79c416f
Rene Dohmen (imac) <acidjunk@gmail.com>
2019-03-22 09:51:44 +0100

    Paginate working

--- src/utils/SubscriptionData.js
+++ src/utils/SubscriptionData.js
@@ -6,11 +6,11 @@ export const requestSubscriptionData = (pageSize, page, sorted, filtered) => {
     console.log(sorted);
     // Todo: log filter and sort OBJECTS also
 
-    return subscriptions(`0,${pageSize}`).then(results => {
+    return subscriptions(`${page*(pageSize-1)},${pageSize+(page*(pageSize-1))}`).then(results => {
 
         const res = {
             rows: results,
-            pages: 3
+            pages: 20 // todo: get it from header
         };
         return res
     });
