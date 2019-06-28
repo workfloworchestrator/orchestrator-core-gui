3f50855d38be136e656060137ff2a0e012edb966
Rene Dohmen (imac) <acidjunk@gmail.com>
2019-06-28 12:00:22 +0200

    Prettified it

--- src/api/index.js
+++ src/api/index.js
@@ -180,7 +180,9 @@ export function subscriptionsByTags(tagList, statusList = []) {
 export function portSubscriptions(tagList, statusList = []) {
     const optionalStatusFilter = `&filter=statuses,${encodeURIComponent(statusList.join("-"))}`;
     return fetchJson(
-        `v2/subscriptions/ports?filter=tags,${encodeURIComponent(tagList.join("-"))}${statusList.length ? optionalStatusFilter : ""}`
+        `v2/subscriptions/ports?filter=tags,${encodeURIComponent(tagList.join("-"))}${
+            statusList.length ? optionalStatusFilter : ""
+        }`
     );
 }
 
