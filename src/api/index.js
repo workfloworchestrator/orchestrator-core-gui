9a923aab985ebc3e5034dba3912fad90c5883aa7
Peter Boers <peter.boers@surfnet.nl>
2019-07-19 10:22:27 +0200

    Revert "Fix node tag selection"
    This reverts commit 3d4df72b0e9cab7e6c5f773812995f88cc86b66a.

--- src/api/index.js
+++ src/api/index.js
@@ -174,7 +174,7 @@ export function subscriptionsByTags(tagList, statusList = []) {
 
 export function nodeSubscriptions(statusList = []) {
     const optionalStatusFilter = `&filter=statuses,${encodeURIComponent(statusList.join("-"))}`;
-    return fetchJson(`v2/subscriptions?filter=product.tag,Node${statusList.length ? optionalStatusFilter : ""}`);
+    return fetchJson(`v2/subscriptions?filter=tags,Node${statusList.length ? optionalStatusFilter : ""}`);
 }
 
 export function portSubscriptions(tagList, statusList = []) {
