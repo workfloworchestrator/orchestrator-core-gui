62b57bd4855d255d1516fc94339bd9ef7379334d
Peter Boers <peter.boers@surfnet.nl>
2020-03-25 08:20:57 +0100

    Handle 503 when submitting a task or process

--- src/pages/NewProcess.tsx
+++ src/pages/NewProcess.tsx
@@ -92,7 +92,7 @@ export default class NewProcess extends React.Component<IProps, IState> {
                 let promise = startProcess(createWorkflow, [{ product: option.value }]);
 
                 let promise2 = catchErrorStatus<EngineStatus>(promise, 503, json => {
-                    setFlash(I18n.t("engine.locked"), "error");
+                    setFlash(I18n.t("settings.status.engine.true"), "error");
                     this.context.redirect("/processes");
                 });
 
