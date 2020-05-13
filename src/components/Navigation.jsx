55490da0bfdf5417cefc66b37144105761cb6494
Rene Dohmen <acidjunk@gmail.com>
2020-05-13 22:57:48 +0200

    Refactored spinner to use hooks (no more deprecated lifecyle methods in Navigation.jsx)

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,18 +1,39 @@
 import "./Navigation.scss";
 
-import React, { useState } from "react";
-import { useLocation } from "react-router-dom";
+import React, { useEffect, useRef, useState } from "react";
+import { Spinner } from "spin.js";
 
+import mySpinner from "../lib/Spin";
 import NavigationItem from "./NavigationItem";
 
 function Navigation() {
-    // const location = useLocation();
-    // console.log(location);
-    // const [value, setValue] = useState(location.pathname);
-    // const onChange = (e, value) => {
-    //     setValue(value);
-    // };
-    // TODO: load/use spinner
+    const [loading, setLoading] = useState(false);
+    const spinnerTarget = useRef();
+    const spinnerElement = useRef();
+
+    useEffect(() => {
+        mySpinner.onStart = () => setLoading(true);
+        mySpinner.onStop = () => setLoading(false);
+    }, []);
+
+    useEffect(() => {
+        if (loading) {
+            if (!spinnerElement.current) {
+                spinnerElement.current = new Spinner({
+                    lines: 25, // The number of lines to draw
+                    length: 12, // The length of each line
+                    width: 2, // The line thickness
+                    radius: 8, // The radius of the inner circle
+                    color: "#4DB3CF", // #rgb or #rrggbb or array of colors
+                    top: "25%",
+                    position: "fixed"
+                }).spin(spinnerTarget.current);
+            }
+        } else {
+            spinnerElement.current = null;
+        }
+    });
+
     return (
         <div className="navigation-container">
             <div className="navigation">
@@ -24,6 +45,7 @@ function Navigation() {
                 <NavigationItem href="/prefixes" value="prefixes" />
                 <NavigationItem href="/settings" value="settings" />
                 <NavigationItem href="/new-process" value="new_process" className="new_process" />
+                {loading && <div className="spinner" ref={spinner => (spinnerTarget.current = spinner)} />}
             </div>
         </div>
     );
