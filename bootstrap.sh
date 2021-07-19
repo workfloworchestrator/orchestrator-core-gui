1b31dd572caae77ada815d7b10480ab79a1d5353
René Dohmen <acidjunk@gmail.com>
2021-07-19 16:45:02 +0200

    Fix some tests and add a "test" mode to bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,8 +2,12 @@
 cd src || exit
 if [ "$1" = "surf" ]; then
     echo "Switching to linked mode; loading SURF specific modules"
-    rm -rf custom
+    rm custom
     ln -s ../../orchestrator-client-surf/src custom
+elif [ "$1" = "test" ]; then
+    echo "Switching to test mode; with SURF specific modules"
+    echo "This is a temporary workaround as jest has problems with symlinks and the tests aren't splitted yet"
+    cp -r ../../orchestrator-client-surf/src custom
 else
     echo "Removing surfnet specific components and reverting back to standalone mode"
     rm custom
--- src/lib/uniforms-surfnet/__tests__/VlanField.tsx
+++ src/lib/uniforms-surfnet/__tests__/VlanField.tsx
@@ -15,10 +15,11 @@
 
 import waitForComponentToPaint from "__tests__/waitForComponentToPaint";
 import mock from "axios-mock";
-import { ListField, VlanField } from "custom/uniforms";
+import VlanField from "custom/uniforms/VlanField";
 import createContext from "lib/uniforms-surfnet/__tests__/_createContext";
 import mount from "lib/uniforms-surfnet/__tests__/_mount";
 import withSubscriptions from "lib/uniforms-surfnet/__tests__/_withSubscriptions";
+import { ListField } from "lib/uniforms-surfnet/src/index";
 import React from "react";
 import { NestField } from "uniforms-unstyled";
 
