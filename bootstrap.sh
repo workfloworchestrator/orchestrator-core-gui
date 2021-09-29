edb6d8b0cb34dbb0bf58fd2c4ced60c48fb9d1ff
Peter Boers <peter.boers@surfnet.nl>
2021-09-29 10:49:30 +0200

    Fix .env.local.example

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 cd src || exit
-if [ "$1" = "surf" ]; then
-    echo "Switching to linked mode; loading SURF specific modules"
-    rm -rf custom
-    ln -s ../../orchestrator-client-surf/src custom
-elif [ "$1" = "test" ]; then
-    echo "Switching to test mode; with SURF specific modules"
-    echo "This is a temporary workaround as jest has problems with symlinks so the tests aren't split yet"
-    rm -rf custom
-    cp -r ../../orchestrator-client-surf/src custom
-else
-    echo "Removing surfnet specific components and reverting back to standalone mode"
-    rm -rf custom
-    ln -s custom-example custom
-fi
+
+echo "Removing surfnet specific components and reverting back to standalone mode"
+rm -rf custom
+ln -s custom-example custom
+
