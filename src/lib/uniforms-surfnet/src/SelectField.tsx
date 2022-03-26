898f55ba2a6fedcb0caa15fdffd6829ba6cef368
René Dohmen <acidjunk@gmail.com>
2022-03-26 01:23:12 +0100

    Dark theme for react select

--- src/lib/uniforms-surfnet/src/SelectField.tsx
+++ src/lib/uniforms-surfnet/src/SelectField.tsx
@@ -18,12 +18,23 @@ import ListField, { ListFieldProps } from "lib/uniforms-surfnet/src/ListField";
 import ListItemField from "lib/uniforms-surfnet/src/ListItemField";
 import { FieldProps } from "lib/uniforms-surfnet/src/types";
 import { get } from "lodash";
-import React from "react";
+import React, { useContext } from "react";
 import { WrappedComponentProps, injectIntl } from "react-intl";
 import ReactSelect from "react-select";
 import { connectField, filterDOMProps, joinName, useField, useForm } from "uniforms";
 // Todo: Not sure about this temp fix for circular import
 import { SelectField } from "uniforms-unstyled";
+import ApplicationContext from "utils/ApplicationContext";
+import {
+    DARK_BACKGROUND_COLOR,
+    DARK_FONT_COLOR,
+    DARK_ROW_BORDER_COLOR,
+    DARK_SELECTED_FONT_COLOR,
+    LIGHT_BACKGROUND_COLOR,
+    LIGHT_FONT_COLOR,
+    LIGHT_ROW_BORDER_COLOR,
+    LIGHT_SELECTED_FONT_COLOR,
+} from "utils/Constants";
 import { Option } from "utils/types";
 
 export type SelectFieldProps = FieldProps<
@@ -52,6 +63,8 @@ function Select({
     intl,
     ...props
 }: SelectFieldProps) {
+    const { theme } = useContext(ApplicationContext);
+
     const nameArray = joinName(null, name);
     let parentName = joinName(nameArray.slice(0, -1));
 
@@ -78,6 +91,32 @@ function Select({
     }));
 
     const selectedValue = options.find((option: Option) => option.value === value);
+
+    const seperatorColor = theme === "light" ? LIGHT_ROW_BORDER_COLOR : DARK_ROW_BORDER_COLOR;
+    const backgroundColor = theme === "light" ? LIGHT_BACKGROUND_COLOR : DARK_BACKGROUND_COLOR;
+    const fontColor = theme === "light" ? LIGHT_FONT_COLOR : DARK_FONT_COLOR;
+    const selectedFontColor = theme === "light" ? LIGHT_SELECTED_FONT_COLOR : DARK_SELECTED_FONT_COLOR;
+
+    const customStyles = {
+        option: (provided: any, state: { isSelected: any }) => ({
+            ...provided,
+            borderBottom: `1px solid ${seperatorColor}`,
+            backgroundColor: backgroundColor,
+            color: state.isSelected ? selectedFontColor : fontColor,
+        }),
+        control: (provided: any) => ({
+            ...provided,
+            backgroundColor: backgroundColor,
+            color: fontColor,
+        }),
+        // TODO: check if we need to style this
+        singleValue: (provided: any, state: { isDisabled: any }) => {
+            const opacity = state.isDisabled ? 0.5 : 1;
+            const transition = "opacity 300ms";
+
+            return { ...provided, opacity, transition, color: fontColor };
+        },
+    };
 
     if (fieldType === Array) {
         return (
@@ -107,6 +146,7 @@ function Select({
                                 onChange(option?.value);
                             }
                         }}
+                        styles={customStyles}
                         options={options}
                         value={selectedValue}
                         isSearchable={true}
