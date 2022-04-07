d45140ef9a8daa24b396705d57129ed435916d7e
Mark90 <mark_moes@live.nl>
2022-04-07 10:02:11 +0200

    Update storyshots according to docs

--- src/stories/__tests__/storyshots.test.ts
+++ src/stories/__tests__/storyshots.test.ts
@@ -13,59 +13,94 @@
  *
  */
 
-import initStoryshots, { Stories2SnapsConverter } from "@storybook/addon-storyshots";
-import { RenderTree } from "@storybook/addon-storyshots/dist/ts3.9/frameworks/Loader";
-import { act } from "react-test-renderer";
+// import initStoryshots, { Stories2SnapsConverter } from "@storybook/addon-storyshots";
+// import { RenderTree } from "@storybook/addon-storyshots/dist/ts3.9/frameworks/Loader";
+// import { act } from "react-test-renderer";
 
-const converter = new Stories2SnapsConverter();
+// const converter = new Stories2SnapsConverter();
 
-// Based on the the async example and the default `snapshot` function from @storybook/addon-storyshots
-// Used story id as snapshot filename
-function AsyncSnapshot({
+// // Based on the the async example and the default `snapshot` function from @storybook/addon-storyshots
+// // Used story id as snapshot filename
+// function AsyncSnapshot({
+//     story,
+//     context,
+//     renderTree,
+//     done, // --> callback passed to test method when asyncJest option is true
+// }: {
+//     story: any;
+//     context: any;
+//     renderTree: RenderTree;
+//     done: () => void;
+// }): Promise<void> | void {
+//     const snapshotFilename = converter.getSnapshotFileName(context);
+
+//     // mount the story
+//     const result = renderTree(story, context, {});
+
+//     async function match(tree: any) {
+//         await act(async () => {
+//             tree.update(story.render());
+//             await new Promise((resolve) => setTimeout(resolve, 2000));
+//         });
+//         const updated_tree = tree.toJSON();
+
+//         if (snapshotFilename) {
+//             expect(updated_tree).toMatchSpecificSnapshot(snapshotFilename);
+//         } else {
+//             expect(updated_tree).toMatchSnapshot();
+//         }
+
+//         if (typeof updated_tree.unmount === "function") {
+//             tree.unmount();
+//         }
+//         done();
+//     }
+
+//     if (typeof result.then === "function") {
+//         return result.then(match);
+//     }
+
+//     return match(result);
+// }
+
+// initStoryshots({
+//     /* configuration options */
+//     asyncJest: true,
+//     stories2snapsConverter: converter,
+//     test: AsyncSnapshot,
+// });
+
+
+import initStoryshots, { Stories2SnapsConverter } from '@storybook/addon-storyshots';
+import { mount } from 'enzyme';
+import toJson from 'enzyme-to-json';
+
+// Runner
+initStoryshots({
+  asyncJest: true, // this is the option that activates the async behaviour
+  test: ({
     story,
     context,
-    renderTree,
     done, // --> callback passed to test method when asyncJest option is true
-}: {
-    story: any;
-    context: any;
-    renderTree: RenderTree;
-    done: () => void;
-}): Promise<void> | void {
+  }) => {
+    const converter = new Stories2SnapsConverter();
     const snapshotFilename = converter.getSnapshotFileName(context);
+    const storyElement = story.render();
 
     // mount the story
-    const result = renderTree(story, context, {});
+    const tree = mount(storyElement);
 
-    async function match(tree: any) {
-        await act(async () => {
-            tree.update(story.render());
-            await new Promise((resolve) => setTimeout(resolve, 2000));
-        });
-        const updated_tree = tree.toJSON();
+    // wait until the mount is updated, in our app mostly by Relay
+    // but maybe something else updating the state of the component
+    // somewhere
+    const waitTime = 1;
+    setTimeout(() => {
+      if (snapshotFilename) {
+        expect(toJson(tree.update())).toMatchSpecificSnapshot(snapshotFilename);
+      }
 
-        if (snapshotFilename) {
-            expect(updated_tree).toMatchSpecificSnapshot(snapshotFilename);
-        } else {
-            expect(updated_tree).toMatchSnapshot();
-        }
-
-        if (typeof updated_tree.unmount === "function") {
-            tree.unmount();
-        }
-        done();
-    }
-
-    if (typeof result.then === "function") {
-        return result.then(match);
-    }
-
-    return match(result);
-}
-
-initStoryshots({
-    /* configuration options */
-    asyncJest: true,
-    stories2snapsConverter: converter,
-    test: AsyncSnapshot,
+      done();
+    }, waitTime);
+  },
+  // other options here
 });
