b4abd9d2ab325c3e6e5200fb872e72a0a83fc4d9
René Dohmen <acidjunk@gmail.com>
2022-06-13 11:51:32 +0200

    Fix timeout bug

--- src/websocketService/index.ts
+++ src/websocketService/index.ts
@@ -18,6 +18,6 @@ class WebsocketService {
     };
 }
 
-export const websocketReconnectTime = 28800000;
+export const websocketReconnectTime = 1740000; // 29 minutes
 export const websocketService = new WebsocketService();
 export { WebSocketCodes };
