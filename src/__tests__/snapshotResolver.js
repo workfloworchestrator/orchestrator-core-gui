5fc8726993f81018ebd0ac754c43c2ef7d4c844d
tjeerddie <tjeerdverschragen@hotmail.com>
2022-09-14 13:59:34 +0200

    Fix snapshots diff of standalone and SURF version
    - snapshots now go into their respective version folder
    - - standalone: __standalone-snapshots__/*
    - - SURF: __SURF-snapshots__/*

---
+++ src/__tests__/snapshotResolver.js
@@ -0,0 +1,32 @@
+import manifest from "custom/manifest.json";
+
+const versionFolder = `__${manifest.name}-snapshots__/`;
+/**
+ *
+ * @param testPath Path of the test file being test3ed
+ * @param snapshotExtension The extension for snapshots (.snap usually)
+ */
+const resolveSnapshotPath = (testPath, snapshotExtension) => {
+    const filePath = testPath.substr(0, testPath.lastIndexOf("/") + 1);
+    const fileName = testPath.substr(testPath.lastIndexOf("/") + 1, testPath.length);
+
+    return `${filePath}${versionFolder}${fileName}${snapshotExtension}`;
+};
+
+/**
+ *
+ * @param snapshotFilePath The filename of the snapshot (i.e. some.test.js.snap)
+ * @param snapshotExtension The extension for snapshots (.snap)
+ */
+const resolveTestPath = (snapshotFilePath, snapshotExtension) => {
+    return snapshotFilePath.replace(snapshotExtension, "").replace(versionFolder, ""); //Remove the .snap
+};
+
+/* Used to validate resolveTestPath(resolveSnapshotPath( {this} )) */
+const testPathForConsistencyCheck = "some.test.js";
+
+module.exports = {
+    resolveSnapshotPath,
+    resolveTestPath,
+    testPathForConsistencyCheck,
+};
