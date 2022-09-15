dea6753aa77a5ff6cbd6be2dd992064ca412a81f
tjeerddie <tjeerdverschragen@hotmail.com>
2022-09-15 09:37:13 +0200

    Change snapshotResolver to typescript

--- src/jestSnapshotResolver/snapshotResolver.ts
+++ src/jestSnapshotResolver/snapshotResolver.ts
@@ -6,9 +6,9 @@ const versionFolder = `__${manifest.name}-snapshots__/`;
  * @param testPath Path of the test file being tested
  * @param snapshotExtension The extension for snapshots (.snap usually)
  */
-const resolveSnapshotPath = (testPath, snapshotExtension) => {
-    const filePath = testPath.substr(0, testPath.lastIndexOf("/") + 1);
-    const fileName = testPath.substr(testPath.lastIndexOf("/") + 1, testPath.length);
+const resolveSnapshotPath = (testPath: string, snapshotExtension: string) => {
+    const filePath = testPath.substring(0, testPath.lastIndexOf("/") + 1);
+    const fileName = testPath.substring(testPath.lastIndexOf("/") + 1, testPath.length);
 
     return `${filePath}${versionFolder}${fileName}${snapshotExtension}`;
 };
@@ -18,7 +18,7 @@ const resolveSnapshotPath = (testPath, snapshotExtension) => {
  * @param snapshotFilePath The filename of the snapshot (i.e. some.test.js.snap)
  * @param snapshotExtension The extension for snapshots (.snap)
  */
-const resolveTestPath = (snapshotFilePath, snapshotExtension) => {
+const resolveTestPath = (snapshotFilePath: string, snapshotExtension: string) => {
     return snapshotFilePath.replace(snapshotExtension, "").replace(versionFolder, ""); //Remove the .snap
 };
 
