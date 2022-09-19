b51d4564a84287004ba1e5951e766c6f661eea57
René Dohmen <acidjunk@gmail.com>
2022-09-19 13:08:35 +0200

    Prepare for Timezone awareness/DST

--- src/custom-surf/Utils.ts
+++ src/custom-surf/Utils.ts
@@ -2,7 +2,7 @@ import moment from "moment";
 
 export function renderStringAsDateTime(datetime: string, short: boolean = false) {
     if (short) {
-        return moment(datetime).zone(-240).format("Do MMM YYYY, HH:mm:ss");
+        return moment(datetime).utc().zone(-240).format("Do MMM YYYY, HH:mm:ss");
     }
     return moment(datetime).zone(-240).format("dddd, MMMM Do YYYY, h:mm:ss a");
 }
