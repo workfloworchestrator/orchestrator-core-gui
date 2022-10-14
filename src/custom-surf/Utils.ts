c0f7d83251ab76e4dca287f2bf8182306e4ccaa4
René Dohmen <acidjunk@gmail.com>
2022-10-15 00:15:42 +0200

    use month name

--- src/custom-surf/Utils.ts
+++ src/custom-surf/Utils.ts
@@ -4,7 +4,7 @@ export function renderStringAsDateTime(datetime: string, short: boolean = false)
     if (short) {
         return moment.utc(datetime).tz("Europe/Amsterdam").format("DD-MM-YYYY HH:mm");
     }
-    return `${moment.utc(datetime).tz("Europe/Amsterdam").format("DD-MM-YYYY HH:mm")} AMS (${moment
+    return `${moment.utc(datetime).tz("Europe/Amsterdam").format("DD-MMM-YYYY HH:mm")} AMS (${moment
         .utc(datetime)
         .format("HH:mm")} UTC)`;
 }
